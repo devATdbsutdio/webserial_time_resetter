1b8a533ed7f7b85855598659801ff1dba16ef2a6
Saurabh Datta <hi@dattasaurabh.com>
2021-08-20 14:43:57 +0800

    migrating from public to docs

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var HOST = process.env.HOST || '';
 
 const app = express()
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static('docs'))
 
 var options = {
     key  : fs.readFileSync('ssl/key.pem'),
